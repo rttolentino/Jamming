2ae9e9b9ffee04e05fd3486e4aabef97ce15fce7
Rob Tolentino <32676605+rttolentino@users.noreply.github.com>
2017-11-30 21:49:14 -0500

    Assessment 5 (#5)
    * Completed 5th assessment, added functionality to "+", fixed formatting
    
    * Updated gitignore file
    
    * Removed .config file from repo

--- src/App.js
+++ src/App.js
@@ -1,29 +1,52 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
 import SearchBar from './components/SearchBar/SearchBar.js';
 import SearchResults from './components/SearchResults/SearchResults.js';
 import Playlist from './components/Playlist/Playlist.js';
 
-class App extends Component {
-  constructor(props) {
+class App extends React.Component
+{
+  constructor(props)
+  {
     super(props);
 
     this.state = {
-      searchResults: [],
+      searchResults: [
+/*
+        {
+          name: `Song 2`,
+          artist: `Blur`,
+          album: `Self-titled`
+        }
+*/
+      ],
       playlistName: ``,
       playlistTracks: []
     };
+
+    this.addTrack = this.addTrack.bind(this);
   }
 
-  render() {
+  addTrack(track)
+  {
+    if(!this.state.playlistTracks.find(t => t.name == track.name))
+    {
+      let tempList = this.state.playlistTracks.slice();
+      tempList.push(track);
+      this.setState({playlistTracks: tempList});
+    }
+  }
+
+  render()
+  {
     return (
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults}/>
+            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
             <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}/>
           </div>
         </div>
--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -3,12 +3,14 @@ import './SearchResults.css';
 
 import Tracklist from '../Tracklist/Tracklist.js';
 
-class SearchResults extends React.Component{
-  render() {
+class SearchResults extends React.Component
+{
+  render()
+  {
     return (
       <div className="SearchResults">
         <h2>Results</h2>
-        <Tracklist tracks={this.props.searchResults}/>
+        <Tracklist tracks={this.props.searchResults} onAdd={this.props.onAdd} />
       </div>
     );
   }
